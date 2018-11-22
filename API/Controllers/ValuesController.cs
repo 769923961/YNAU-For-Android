96c186e7ac1de04a35cf46be0e3bc8c057e46f93
WangY <769923961@qq.com>
2018-11-22 16:37:09 +0800

    取消自带验证

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Common;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Model;
 
 namespace API.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -30,33 +27,32 @@ namespace API.Controllers
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
-        }
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
-            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
         }
+
         [HttpPost]
-        public void Login(User user)
+        public void Post()
         {
             HttpHelper http = new HttpHelper();
             HttpItem item = new HttpItem();
             item.URL = "http://202.203.187.86:9000/LoginTeach.aspx";
             item.Postdata = "uid=2015310687&pwd=46480113&type=student";
-            http.GetHtml(item);
+            var a =http.GetHtml(item);
         }
     }
 }
+
