e158623b9269070fc151ffee9add2f60c5458bdb
WangY <769923961@qq.com>
2018-11-22 16:12:50 +0800

    添加用户登录测试

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace API.Controllers
 {
@@ -45,6 +48,15 @@ namespace API.Controllers
         public void Delete(int id)
         {
             // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        }
+        [HttpPost]
+        public void Login(User user)
+        {
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem();
+            item.URL = "http://202.203.187.86:9000/LoginTeach.aspx";
+            item.Postdata = "uid=2015310687&pwd=46480113&type=student";
+            http.GetHtml(item);
         }
     }
 }
---
+++ Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Model
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string Type { get; set; }
+
+    }
+}
